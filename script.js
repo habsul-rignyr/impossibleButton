35af4eeba956c96216e2a7d8fe851078626903e1
josh612 <joshamy@gmail.com>
2023-02-03 14:44:54 +0000

    Added a third exercise, which is capturing the event from a keypress and using it to add items to a list.

--- script.js
+++ script.js
@@ -12,20 +12,46 @@
 // 	document.body.style.backgroundColor = '#00FF00';
 // })
 
-const colors = ["red", "blue", "green", "yellow", "purple", "pink", "brown", "orange", "grey", "black"];
+// const colors = ["red", "blue", "green", "yellow", "purple", "pink", "brown", "orange", "grey", "black"];
 
-const changeColor = function(){
-	const h1 = document.querySelector('h1');
-	h1.style.color = this.style.backgroundColor;
+// const changeColor = function(){
+// 	const h1 = document.querySelector('h1');
+// 	h1.style.color = this.style.backgroundColor;
 	
-}
+// }
 
-const container = document.querySelector('#boxes');
+// -------------------Second exercise------------------
 
-for (let color of colors) {
-	const box = document.createElement('div');
-	box.style.backgroundColor = color;
-	box.classList.add('box');
-	container.appendChild(box);
-	box.addEventListener('click', changeColor);
+// const container = document.querySelector('#boxes');
+
+// for (let color of colors) {
+// 	const box = document.createElement('div');
+// 	box.style.backgroundColor = color;
+// 	box.classList.add('box');
+// 	container.appendChild(box);
+// 	box.addEventListener('click', changeColor);
+// }
+
+// -------------------Third exercise------------------
+const input = document.querySelector('#username');
+
+input.addEventListener('keydown', function(e){
+	console.log('Key Down!');
+})
+
+const addItemInput = document.querySelector('#addItem');
+const itemsUL = document.querySelector('#items');
+
+addItemInput.addEventListener('keypress', function(e){
+if (e.key === 'Enter') {
+	//add new item to list
+	
+	const newItem = document.createElement('li');
+	newItem.innerHTML = this.value;
+	itemsUL.append(newItem);
+	this.value = '';
 }
+	
+});
+
+document.body.append(itemsUL);
