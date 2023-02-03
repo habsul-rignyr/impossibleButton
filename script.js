57c275353ab9b4118b55e5d92c55c138759d187a
josh612 <joshamy@gmail.com>
2023-02-03 12:33:43 +0000

    Further practice with event listeners by creating a loop over a list of colors; clicking them changes the h1 to the same color.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
-btn.addEventListener('mouseover', function() {
-	const height = Math.floor(Math.random() * window.innerHeight);
-	const width = Math.floor(Math.random() * window.innerWidth);
-	btn.style.left = `${width}px`;
-	btn.style.top = `${height}px`;
-});
+// btn.addEventListener('mouseover', function() {
+// 	const height = Math.floor(Math.random() * window.innerHeight);
+// 	const width = Math.floor(Math.random() * window.innerWidth);
+// 	btn.style.left = `${width}px`;
+// 	btn.style.top = `${height}px`;
+// });
 
-btn.addEventListener('click', function(){
-	btn.innerText = 'YOU GOT ME!!!';
-	document.body.style.backgroundColor = '#00FF00';
-})
+// btn.addEventListener('click', function(){
+// 	btn.innerText = 'YOU GOT ME!!!';
+// 	document.body.style.backgroundColor = '#00FF00';
+// })
+
+const colors = ["red", "blue", "green", "yellow", "purple", "pink", "brown", "orange", "grey", "black"];
+
+const changeColor = function(){
+	const h1 = document.querySelector('h1');
+	h1.style.color = this.style.backgroundColor;
+	
+}
+
+const container = document.querySelector('#boxes');
+
+for (let color of colors) {
+	const box = document.createElement('div');
+	box.style.backgroundColor = color;
+	box.classList.add('box');
+	container.appendChild(box);
+	box.addEventListener('click', changeColor);
+}
